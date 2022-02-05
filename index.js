4c7ed76cd1d076fb5e2f27380b34ff206ac4d2ff
mckaymatt3 <mckaymatt@outlook.com>
2022-02-04 22:51:23 -0500

    Complted Assignment

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = (array) => {const newArray = array.map()
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
+  const tutorialSplit = tutorials.map(function(item) {
+    let newItem = item.split(" ")
+    console.log('This is the split string sentence: ', newItem)
+    let tutorialUpper = newItem.map(function(item2) {
+      let newItem2 = item2.slice(0,1).toUpperCase() + item2.slice(1)
+        return newItem2
+    })
+    console.log('This is the upper case split string: ', tutorialUpper)
+    return tutorialUpper.join(' ')
+  })
+  console.log('This is the final result: ',tutorialSplit)
+  return tutorialSplit
 }
+
+// console.log(titleCased(tutorials))
+
